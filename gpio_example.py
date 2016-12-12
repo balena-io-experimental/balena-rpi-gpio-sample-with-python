6e9991312b831a71cb91a2e2ebce8076fa1de317
Cameron Diver <cameron@resin.io>
2016-12-12 13:08:31 +0000

    Updated Dockerfile to Dockerfile.template and added some README information.
    Also addded try...finally to python code to ensure GPIO cleanup.

--- gpio_example.py
+++ gpio_example.py
@@ -2,9 +2,6 @@
 
 import RPi.GPIO as GPIO
 import time
-#import logging
-
-#logging.basicConfig(format='%(levelname)s-%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG,filename='/App/gpio.log')
 
 # Set GPIO mode: GPIO.BCM or GPIO.BOARD
 GPIO.setmode(GPIO.BOARD)
@@ -17,16 +14,17 @@ gpioList2 = [16,18,19,21,22,23,24,26]
 GPIO.setup(gpioList1, GPIO.OUT)
 GPIO.setup(gpioList2, GPIO.OUT)
 
-while True:
-	# Change gpio pins in list 1 from low to high and list 2 from high to low
-	GPIO.output(gpioList1, 1)
-	GPIO.output(gpioList2, 0)
-	time.sleep(1)
+try:
+	while True:
+		# Change gpio pins in list 1 from low to high and list 2 from high to low
+		GPIO.output(gpioList1, 1)
+		GPIO.output(gpioList2, 0)
+		time.sleep(1)
 
-	# Change gpio pin in list 1 from high to low and list 2 from low to high
-	GPIO.output(gpioList1, 0)
-	GPIO.output(gpioList2, 1)
-	time.sleep(1)
-
-# Reset all gpio pin
-GPIO.cleanup()
+		# Change gpio pin in list 1 from high to low and list 2 from low to high
+		GPIO.output(gpioList1, 0)
+		GPIO.output(gpioList2, 1)
+		time.sleep(1)
+finally:
+	# Reset all gpio pin
+	GPIO.cleanup()
